802e7cb038f9d13bf60a3387295343d99fece503
FahadAminShovon <shovonfahad991@gmail.com>
2021-09-18 17:37:36 +0600

    chapter(Depeloping): composing configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const { mode } = require('webpack-nano/argv');
-const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin');
-const { WebpackPluginServe } = require('webpack-plugin-serve');
+const { merge } = require('webpack-merge');
+const parts = require('./webpack.parts');
 
-module.exports = {
-	watch: mode === 'development',
-	entry: ['./src', 'webpack-plugin-serve/client'],
-	mode,
-	plugins: [
-		new MiniHtmlWebpackPlugin({
-			context: { title: 'Demo' },
-		}),
-		new WebpackPluginServe({
-			host: '127.0.0.1',
-			port: process.env.PORT || 8080,
-			static: './dist',
-			liveReload: true,
-			waitForBuild: true,
-		}),
-	],
+const commonConfig = merge([
+	{ entry: ['./src'] },
+	parts.page({ title: 'Demo' }),
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+	{ entry: ['webpack-plugin-serve/client'] },
+	parts.devServer(),
+]);
+
+const getConfig = (mode) => {
+	switch (mode) {
+		case 'production':
+			return merge(commonConfig, productionConfig, { mode });
+		case 'development':
+			return merge(commonConfig, developmentConfig, { mode });
+		default:
+			throw new Error(`Trying to use an unknown mode ${mode}`);
+	}
 };
+
+module.exports = getConfig(mode);
---
+++ webpack.parts.js
@@ -0,0 +1,19 @@
+const { WebpackPluginServe } = require('webpack-plugin-serve');
+const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin');
+
+exports.devServer = () => ({
+	watch: true,
+	plugins: [
+		new WebpackPluginServe({
+			host: '127.0.0.1',
+			port: process.env.PORT || 8080,
+			static: './dist',
+			liveReload: true,
+			waitForBuild: true,
+		}),
+	],
+});
+
+exports.page = ({ title }) => ({
+	plugins: [new MiniHtmlWebpackPlugin({ context: { title } })],
+});
